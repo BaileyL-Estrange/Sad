3d1632491638dffa28941cede8ca0c02bd7d01f9
Bailey <734019@newcollege.ac.uk>
2026-01-15 12:10:12 +0000

    changing code and input actions

--- Assets/SCRIPTS/PlayerMovementStats.cs
+++ Assets/SCRIPTS/PlayerMovementStats.cs
@@ -47,4 +47,24 @@ public class PlayerMovementStats : ScriptableObject
     public bool DrawRight = true;
     [Range(5, 100)] public int ArcResolution = 20;
     [Range(0, 500)] public int VisualizationSteps = 90;
+
+
+    public float Gravity { get; private set; }
+    public float InitialJumpVelocity { get; private set; }
+    public float AdjustedJumpHeight { get; private set; }
+
+    private void OnValidate()
+    {
+        CalculateValues();
+    }
+    private void OnEnable()
+    {
+        CalculateValues();
+    }
+    private void CalculateValues()
+    {
+        AdjustedJumpHeight = JumpHeight * JumpHeightCompensationFactor;
+        Gravity = -(2f * AdjustedJumpHeight) / Mathf.Pow(TimeTillJumpApex, 2f);
+        InitialJumpVelocity = Mathf.Abs(Gravity) * TimeTillJumpApex;
+    }
 }
